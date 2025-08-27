20a3e9312c425c7e030b025e2e7f15d79fc2f2df
Dilkhushnapit <150137189+Dilkhushnapit@users.noreply.github.com>
2025-08-28 04:06:13 +0530

    Update RelatedDoctors.jsx
    try to deploy

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { AppContext } from '../context/Appcontext'
+import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
