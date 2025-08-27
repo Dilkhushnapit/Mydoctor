a520fb7366cc5810e1035150861a77be21e851da
Dilkhushnapit <150137189+Dilkhushnapit@users.noreply.github.com>
2025-08-28 03:25:32 +0530

    Update AppContext.jsx
    try to deploy project

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl =  'https://mydoctor-backend-1vlk.onrender.com';//  import.meta.env.VITE_BACKEND_URL 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
