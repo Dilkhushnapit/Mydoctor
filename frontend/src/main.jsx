a0aadaffe8b953504a4b5ecb42366cc959e6a747
Dilkhushnapit <150137189+Dilkhushnapit@users.noreply.github.com>
2025-08-28 03:48:42 +0530

    Update main.jsx
    solve error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css' 
-import AppContextProvider from './context/Appcontext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AppContextProvider>
