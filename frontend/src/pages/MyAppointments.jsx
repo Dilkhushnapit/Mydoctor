de694c9e411be8c7cbee40da4ef582a24c7011a6
Dilkhushnapit <150137189+Dilkhushnapit@users.noreply.github.com>
2025-08-28 04:04:10 +0530

    Update MyAppointments.jsx
    try to deploy

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,5 +1,5 @@
 import React, { use, useContext, useState,useEffect } from 'react'
-import { AppContext } from '../context/Appcontext'
+import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
